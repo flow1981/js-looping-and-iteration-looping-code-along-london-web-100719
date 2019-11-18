69479b3b18d508f256bda88985652f1087840c08
flow1981 <contact.f.stagliano@gmail.com>
2019-11-18 15:01:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+
+    let result = []
+
+    for (let i = 0; i < names.length; i++) {
+    result.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return result
+}
+// const list = ['Tom', 'Pete', 'Mike'];
+// const event = "birthday";
+
+// a = writeCards(list, event);
+
+// console.log(a)
+
+
+function countDown(start) {
+
+    let i = 0
+    let number = start
+
+    while (i<=start) {
+
+        console.log(number)
+        number -= 1
+        i++
+    }
+
+}
+
+countDown(10)
